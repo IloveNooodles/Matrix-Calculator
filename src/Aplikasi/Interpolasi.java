181bfe66e01cefc663dc1b96aaeff3a5a9611373
Aira Thalca <13520101@std.stei.itb.ac.id>
2021-09-23 10:11:30 +0700

    add interpolasi

---
+++ src/Aplikasi/Interpolasi.java
@@ -0,0 +1,30 @@
+package Aplikasi;
+
+import java.util.Scanner;
+
+import Matrix.Matrix;
+
+public class Interpolasi {
+    public static void interpolasi(int n){
+        Matrix a = new Matrix(n, n+1);
+        Scanner sc = new Scanner(System.in);
+        for (int i=0;i<n;i++){
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+            a.setElmt(i, n, y);
+            for (int j=0;j<n;j++){
+                a.setElmt(i, j, Math.pow(x, j));
+            }
+        }
+        Matrix b = new Matrix();
+        b = SistemPersamaanLinear.SPLGaussJordan(a);
+        for (int k=0;k<n;k++){
+            if(k==0){
+                System.out.print(String.format("%.4f", b.getElmt(k,n)));
+            }
+            else{
+                System.out.print(String.format("+ " + "%.4f", b.getElmt(k, n)) + "x^" + k);
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,7 @@ import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args) {
-    Matrix a = new Matrix(4, 4);
-    Matrix b = new Matrix(4, 1);
-    a.createMatrix();
-    b.createMatrix();
-    Crammer.crammer(a, b);
+    int n = 3;
+    Interpolasi.interpolasi(n);
   }
 }
