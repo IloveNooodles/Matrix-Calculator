d96ad04dc1a68e36be5b033622ce1b1b72199ae3
IloveNooodles <mgarebaldhie80@gmail.com>
2021-09-23 10:52:22 +0700

    adding class to bin

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import Aplikasi.*;
 import java.io.*;
 import java.util.Scanner;
 
+import javax.swing.text.Utilities;
+
 public class Main{
   public static void main(String[] args) {
-    int n = 3;
-    Interpolasi.interpolasi(n);
+    Menu.mainMenu();
   }
 }
