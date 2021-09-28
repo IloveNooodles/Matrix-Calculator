323c7c9e2fe7bc6b53de984233040abe886b1903
IloveNooodles <mgarebaldhie80@gmail.com>
2021-09-28 21:57:27 +0700

    fix precision again

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.io.File;
+
+import Aplikasi.RLB;
+import Matrix.*;
 import Utility.*;
 
 public class Main{
   public static void main(String[] args) {
-    Menu.menuLoop();
+    // Menu.menuLoop();
+    Matrix a = new Matrix();
+    a = IO.readMatrix("regresi.txt");
+    System.out.println(Operation.sumCol(a, 0));
+    a = RLB.convertRLBMatrix(a);
+    a.displayMatrix();
   }
 }
